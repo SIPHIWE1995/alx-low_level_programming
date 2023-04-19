6ef7f758fa85a1a48dc554f35deaff2efef388c4
SIPHIWE1995 <siphiwencane21@gmail.com>
2023-04-19 10:34:34 -0700

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,3 +4,19 @@ void print_name(char *name, void (*f)(char *))
 {
     (*f)(name);
 }
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+    // implementation omitted for brevity
+}
+
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    return 0;
+}
